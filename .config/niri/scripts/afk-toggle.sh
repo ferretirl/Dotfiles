56879b4adbf19607be5179dc3d618a74151c2119
Ferret <ferret@cachy>
2026-05-18 00:26:07 +0200

    /

--- .config/niri/scripts/afk-toggle.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-if pgrep -f "sleep infinity" > /dev/null; then
-    kill $(pgrep -f "sleep infinity")
-    kill $(pgrep -f "robloxclicker.sh")
-else
-    systemd-inhibit --what=idle:sleep --who="Roblox AFK" --why="overnight farming" sleep infinity &
-    bash ~/Documents/robloxclicker.sh &
-fi
